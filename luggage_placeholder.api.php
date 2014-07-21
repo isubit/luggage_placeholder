0c63920baddd27cd1e4c6ee57e8cc363a2d4f397
rlfrahm <frahmryan@gmail.com>
2014-07-21 15:45:23 -0500

    LUGG-84 : #time 2h #comment Created feature that exposes a placeholder field in a field configuration #resolved

---
+++ luggage_placeholder.api.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ */
+
+function hook_luggage_placeholder_info() {
+  return array(
+    'text_texfield' => 'value',
+    'number' => 'value',
+    'email_textfield' => 'email',
+  );
+}
